5f93122118de2bffe4dc39735956c0bc3dba3ef3
afmika <afmichael73@gmail.com>
2024-02-11 21:51:53 +0300

    feat: handle rot4x4

--- rust/src/program.rs
+++ rust/src/program.rs
@@ -1,13 +1,23 @@
 use std::{fs, path::Path};
 use anyhow::{bail, Context, Result};
 use core::fmt::Debug;
-use crate::system::System;
+use crate::{mat::{rot_plane, Axis}, system::System};
 
 struct Program {
     source: Vec<u8>,
     system: System,
-    stack: Vec<f32>
+    stack: Vec<f32>,
+    pos: usize
 }
+
+static PLANE_DEF: &[(Axis, Axis); 6] = &[
+    (Axis::X, Axis::Y),
+    (Axis::X, Axis::Z),
+    (Axis::X, Axis::W),
+    (Axis::Y, Axis::Z),
+    (Axis::Y, Axis::W),
+    (Axis::Z, Axis::W),
+]; 
 
 impl Program {
     pub fn new() -> Self {
@@ -15,12 +25,14 @@ impl Program {
             source: vec![],
             system: System::new(),
             stack: vec![],
+            pos: 0
         }
     }
 
     pub fn reset(&mut self) {
         self.system = System::new();
         self.stack = vec![];
+        self.pos = 0;
     }
 
     pub fn load_file<P: AsRef<Path> + Debug + Clone>(&mut self, path: P) -> Result<()> {
@@ -31,20 +43,80 @@ impl Program {
         Ok(())
     }
 
-    pub fn run(&self) -> Result<()> {
-        let mut pos: usize = 0;
-        while pos < self.source.len() {
-            match self.source[pos] as char {
-                '(' => {
-                    pos += 1;
-                    let _op = self.source[pos] as char;
-                    todo!("rotate and update x, y reg on the go")
-                },
-                '{' => todo!("loop closing after ?x|y}}"),
-                '[' => todo!("pop, io"),
-                'x' | 'y' => todo!("push x, y"), 
-                _ => {}
+    pub fn run(&mut self) -> Result<()> {
+        while self.pos < self.source.len() {
+            self.next_state()?;
+        }
+        Ok(())
+    }
+
+    fn next_state(&mut self) -> Result<()> {
+        match self.curr() {
+            '(' => self.handle_rotation(),
+            '{' => self.handle_loop(),
+            '[' => todo!("pop, io"),
+            'x' | 'y' => todo!("push x, y"), 
+            _ => Ok(())
+        }
+    }
+
+    fn handle_rotation(&mut self) -> Result<()> {
+        self.next_char('(')?;
+        let op = self.curr();
+        let mut planes = vec![];
+        while self.curr() != ')' {
+            let c = self.curr();
+            if c >= '0' && c <= '5' {
+                planes.push(c);
+            } else {
+                match c {
+                    '<' | '>' => {
+                        if planes.len() == 0 {
+                            bail!(format!("Expected a plane index 0 .. 5 but got directly a {:?}, at pos {}", c, self.pos));
+                        }
+                        for plane in planes.iter() {
+                            let ascii_index = plane.clone() as usize;
+                            let offset = '0' as usize;
+                            let (u, v) = PLANE_DEF[ascii_index - offset].clone();
+                            let rot4x4 = rot_plane(u, v, if c == '<' { true } else { false })?;
+                            self.system.apply(rot4x4);
+                            let pair = self.system.active_plane_signature();
+                            self.system.apply_signature(pair, op);
+                        }
+                        planes.clear();
+                    },
+                    _ => bail!(format!("Unrecognized token or axis index {:?}, at pos {}", c, self.pos))
+                }
             }
+        }
+
+        todo!("rotate and update x, y reg on the go")
+    }
+
+    fn handle_loop(&mut self) -> Result<()> {
+        self.next_char('{')?;
+        todo!("nested")
+    }
+
+    fn curr(&self) -> char {
+        self.source[self.pos] as char
+    }
+
+    fn jump(&mut self, pos: usize) {
+        self.pos = pos;
+    }
+
+    fn next(&mut self) {
+        self.pos += 1;
+        while self.curr().is_whitespace() {
+            self.next();
+        }
+    }
+
+    fn next_char(&mut self, c: char) -> Result<()> {
+        self.next();
+        if self.curr() != c {
+            bail!(format!("{:?} was expected at pos {}", c, self.pos));
         }
         Ok(())
     }
--- rust/src/system.rs
+++ rust/src/system.rs
@@ -3,12 +3,16 @@ use crate::mat::{Mat2x4, Mat4x1, Mat4x4};
 pub struct System {
   /// Encodes the 4D space state
   core: Mat4x4,
+  acc_x: f32,
+  acc_y: f32,
 }
 
 impl System {
     pub fn new() -> Self {
         Self {
             core: Mat4x4::identity(),
+            acc_x: 0.,
+            acc_y: 0.,
         }
     }
 
@@ -41,6 +45,21 @@ impl System {
         // 2. f filters the first two rows of the resulting 4D vector 
         let ret = f * (t * s);
         (ret[0], ret[1]) 
+    }
+
+    pub fn apply_signature(&mut self, (x, y): (i8, i8), op: char) {
+        let binop = |a: f32, b: f32| -> f32 {
+            match op {
+                '+' => a + b,
+                '-' => a - b,
+                '*' => a * b,
+                '/' => a / b,
+                '_' => a,
+                _ => a
+            }
+        };
+        self.acc_x = binop(self.acc_x, x as f32);
+        self.acc_y = binop(self.acc_y, y as f32);
     }
 
     pub fn display(&self) {
